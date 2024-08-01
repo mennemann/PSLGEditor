f948ad8d50774c370e57155fa00ff68df6105e25
Marcin Mennemann <marcin@mennemann.net>
2024-08-01 10:46:25 +0200

    added xzwasm library + added basic file handling with optional decompression

--- io.js
+++ io.js
@@ -1,17 +1,40 @@
 function importPSLG() {
-    let input = document.createElement("input");
-    input.type = "file";
-    input.click();
+  let input = document.createElement("input");
+  input.type = "file";
+  input.click();
 
-    input.onchange = (e) => {
-        const file = e.target.files[0];
-        file.text().then((text) => {
-            console.log(text);
-        });
-    };
-    alert("this feature will be implemented soon");
+  input.onchange = async (e) => {
+    const file = e.target.files[0];
+
+    if (!file) {
+      return;
+    }
+
+    try {
+      let text;
+
+      if (file.name.endsWith(".json.xz")) {
+        text = await new Response(
+          new xzwasm.XzReadableStream(file.stream()),
+        ).text();
+      } else if (file.name.endsWith(".json")) {
+        text = await file.text();
+      } else {
+        throw new Error("wrong file ending");
+      }
+
+      let content = JSON.parse(text);
+
+      console.log(file.name, text, content);
+    } catch (error) {
+      alert(
+        "Error reading file. Read the section about importing and exporting for further information",
+      );
+      console.log(error);
+    }
+  };
 }
 
 function exportPSLG() {
-    alert("this feature will be implemented soon");
+  alert("this feature will be implemented soon");
 }
